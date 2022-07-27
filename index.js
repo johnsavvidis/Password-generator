8e16377607d4c2bd3f3dfa9a3a5799270708d443
johnsavvidis <savvidisjohn1990@yahoo.gr>
2022-07-27 18:22:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ let passwordEl1 = document.getElementById("password-el1")
 let passwordEl2 = document.getElementById("password-el2")
 
 function PasswordGenerator(){
-    let message1 = " "
-    let message2 = " "
+    let message1 = ""
+    let message2 = ""
     let password1 = [ ]
     let password2 = [ ]
     for (let i = 0; i < 15; i++){
