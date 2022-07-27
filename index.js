a4ab4fe14f01297fa6563b5b903ff2aa1c6b61de
johnsavvidis <savvidisjohn1990@yahoo.gr>
2022-07-27 18:26:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-
 let passwordEl1 = document.getElementById("password-el1")
 let passwordEl2 = document.getElementById("password-el2")
+let message1 = ""
+let message2 = ""
 
 function PasswordGenerator(){
-    let message1 = ""
-    let message2 = ""
     let password1 = [ ]
     let password2 = [ ]
+    passwordEl1.textContent = ""
+    passwordEl2.textContent = ""
     for (let i = 0; i < 15; i++){
         let randomChar1 = Math.floor(Math.random()*characters.length)
         password1[i] = characters[randomChar1]
